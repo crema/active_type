a1a5a6696db64f3b233e9eaa4eb4c6413c0a011b
Joon Min <joongi.min@gmail.com>
2016-06-23 17:37:26 +0900

    FIX - Make ActiveType::Object work well with money-rails
    # Reason
    - money-rails uses #write_attribute to store attribute
    - ActiveType::Object#write_attribute is called on ActiveRecord which complains about non-existing column
    
    # Changes
    - overrided ActiveRecord::AttributeMethods::Dirty#write_attribute for ActiveType::Object

--- lib/active_type/object.rb
+++ lib/active_type/object.rb
@@ -3,13 +3,13 @@ require 'active_type/virtual_attributes'
 require 'active_type/nested_attributes'
 
 module ActiveType
-
   class Object < ActiveRecord::Base
-
     include NoTable
     include VirtualAttributes
     include NestedAttributes
 
+    def write_attribute(attr, value)
+      write_virtual_attribute(attr, value)
+    end
   end
-
 end
